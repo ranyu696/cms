f01c60a59b01d5e07a099c787ef226e513e2b9f1
ranyu <ranyu696@gmail.com>
2024-09-24 18:43:52 +0800

    更新图片加载逻辑

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
     reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
     experimental: {
         serverComponentsExternalPackages: ['sharp'],
       },
